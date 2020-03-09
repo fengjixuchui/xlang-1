6d887453f6bbdd2a71b1b3ff4b67a3004909ea76
qingyiwebt <3211468886@qq.com>
2020-03-09 19:05:32 +0800

    咸鱼化，今天几乎什么都没动

--- main.cpp
+++ main.cpp
@@ -8,16 +8,22 @@
 #include "lexer.h"
 #include "ast.h"
 using namespace std;
+void printVersion(){
+    cout<< "XLang" << endl;
+}
 int main(int argc,char *argv[]){
     vector<string> sources;
     for (int i = 1; i < argc; ++i) {
         string element = argv[i];
         bool handled = false;
+        if(element == "-v" || element == "--version"){
+            printVersion();
+        }
         if (!handled){
             //如果参数没有被处理
             if(element.substr(0,1) == "-"){
                 //以-开头的报未知参数
-                cerr << "Unknown Argument " << element;
+                cerr << "Unknown Argument " << element << endl;
             }else{
                 //否则当做源代码文件处理
                 sources.push_back(element);
